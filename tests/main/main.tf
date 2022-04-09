8400152d9ab2b870f58a42fd3d9a542c8773a32a
alexbasista <abasista@hashicorp.com>
2022-04-09 16:10:11 -0400

    rename dir

---
+++ tests/main/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.8.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "certbotter" {
+  source = "../.."
+
+  cert_fqdn           = var.cert_fqdn
+  cert_email          = var.cert_email
+  route53_hosted_zone = var.route53_hosted_zone
+  output_bucket       = var.output_bucket
+}
