bea46801568b7fb7105f77158ff92882462d8bae
alexbasista <abasista@hashicorp.com>
2024-03-17 12:09:38 -0400

    skip

--- deploy/main.tf
+++ deploy/main.tf
@@ -17,6 +17,7 @@ provider "aws" {
 
 provider "azurerm" {
   environment = "public"
+  skip_provider_registration = true
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
